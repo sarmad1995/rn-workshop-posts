a4bece9ca95b91ea34d7a904924fcafb81b8c9f2
sarmad1995 <azaabudeen@gmail.com>
2020-07-05 17:35:01 +0530

    Added safearea view

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { SafeAreaView } from "react-native";
 import PostList from "./src/components/PostList";
 
 const App = () => {
-  return <PostList />;
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <PostList />
+    </SafeAreaView>
+  );
 };
 
 export default App;
