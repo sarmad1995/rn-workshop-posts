a971d9c826f4c4736bbd9362253ca4a91c7fa076
sarmad1995 <azaabudeen@gmail.com>
2020-07-05 15:36:26 +0530

    Added basic stylying to listtile

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const App = () => {
   return (
     <View>
-      <Text>Helloworld</Text>
+      <View style={styles.listTileContainer}>
+        <Text>Title</Text>
+        <Text>Body</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  listTileContainer: {
+    backgroundColor: "red",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+});
 export default App;
