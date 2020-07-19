aa176223cdf774b93d519a098ffe34eeb8daa60d
sarmad1995 <azaabudeen@gmail.com>
2020-07-19 18:58:42 +0530

    Added rnconfig

--- Bare/App.js
+++ Bare/App.js
@@ -1,8 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { Alert } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  useEffect(() => {
+    Alert.alert(process.env.APP_NAME);
+  }, []);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -14,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
